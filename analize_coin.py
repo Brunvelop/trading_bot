7e412a78afcea24bb4c62f618ebea273165c4dcd
Brunvelop <brunvelop@gmail.com>
2023-09-09 15:55:24 +0200

    Buy done

--- analize_coin.py
+++ analize_coin.py
@@ -6,21 +6,14 @@ import pandas as pd
 
 import yfinance as yf
 
-def download_currency_data(currency, days_to_download=30, interval='1m'):
+def download_currency_data(currency='BTC', days_to_download=30, interval='1h'):
     end = datetime.today()
-    start = end - timedelta(days=7) if interval == '1m' else end - timedelta(days=730)
-    data = pd.DataFrame()
-
-    while (end - start).days <= days_to_download:
-        downloaded_data = yf.download(f'{currency}-USD', start=start, end=end, interval=interval)
-        if downloaded_data.empty:
-            print(f"Error occurred: No data was downloaded for {currency}")
-            break
-        data = pd.concat([data, downloaded_data])
-        end = start
-        start = end - timedelta(days=7 if interval == '1m' else 730)
-
-    data = data.drop_duplicates().sort_index()
+    start = end - timedelta(days=days_to_download)
+    data = yf.download(f'{currency}-USD', start=start, end=end, interval=interval)
+    if data.empty:
+        print(f"Error occurred: No data was downloaded for {currency}")
+    else:
+        data = data.drop_duplicates().sort_index()
     return data
 
 
@@ -33,17 +26,14 @@ def calculate_strategy_1(data):
     data['Sell_Signal'] = (data['Close'] > data['MA200'])
     return data
 
-def backtest(data, usd_balance=10000.0, coin_balance=0.0, buy_amount=100):
+def backtest(data, usd_balance=10000.0, coin_balance=0.0, buy_amount=10):
     trade_history = []
-    fee_rate = 0.0012  # 0.12% fee
     order_id = 0
     open_orders = []
 
     for i, row in tqdm(data.iterrows(), total=data.shape[0]):
-        fee = buy_amount * fee_rate
         # If there is a buy signal, we buy one unit of coin
-        if row['Buy_Signal'] and usd_balance >= (buy_amount + fee):
-            buy_amount -= fee
+        if row['Buy_Signal'] and usd_balance >= buy_amount:
             coin_amount = buy_amount/row['Close']
             usd_amount = buy_amount
 
@@ -55,7 +45,7 @@ def backtest(data, usd_balance=10000.0, coin_balance=0.0, buy_amount=100):
                 'buy_price': row['Close'], 
                 'buy_amount': coin_amount, 
                 'buy_cost': usd_amount, 
-                'buy_fees': fee, 
+                'buy_fees': 0, 
                 'closed': False
             }
             trade_history.append(trade)
@@ -69,15 +59,14 @@ def backtest(data, usd_balance=10000.0, coin_balance=0.0, buy_amount=100):
                 if row['Close'] > trade['buy_price'] * 1.02:
                     coin_amount = trade['buy_amount']
                     usd_amount = coin_amount * row['Close']
-                    fee = usd_amount * fee_rate
-                    usd_balance += usd_amount - fee
+                    usd_balance += usd_amount
                     coin_balance -= coin_amount
                     trade.update({
                         'sell_timestamp': row.name,
                         'sell_price': row['Close'],
                         'sell_amount': coin_amount,
                         'sell_cost': usd_amount,
-                        'sell_fees': fee,
+                        'sell_fees': 0,
                         'closed': True
                     })
                     open_orders.remove(trade)
@@ -86,8 +75,8 @@ def backtest(data, usd_balance=10000.0, coin_balance=0.0, buy_amount=100):
     trade_history = pd.DataFrame(trade_history)
     return usd_balance, coin_balance, trade_history
 
-def plot_trade_history(trade_history, coin_data_1h, coin):
-    fig, axs = plt.subplots(3, 1, figsize=(10, 18))
+def plot(trade_history, coin_data_1h, coin):
+    fig, axs = plt.subplots(4, 1, figsize=(10, 24))  # Increase the number of subplots
     fig.suptitle('Trade History Over Time')
 
     # Filter closed trades
@@ -111,14 +100,67 @@ def plot_trade_history(trade_history, coin_data_1h, coin):
     axs[2].grid(True)
     axs[2].legend()
 
+    # Calculate balances
+    usd_balance = 10000 - trade_history['buy_cost'].cumsum() + trade_history['sell_cost'].cumsum()
+    coin_balance = trade_history['buy_amount'].cumsum() - trade_history['sell_amount'].cumsum()
+
+    # Filter rows with valid sell_timestamp
+    valid_sell_timestamps = trade_history['sell_timestamp'].notna()
+
+    # New subplot for balance evolution
+    axs[3].plot(trade_history['buy_timestamp'], usd_balance, label='USD Balance Over Time', color='purple')
+    axs[3].plot(trade_history.loc[valid_sell_timestamps, 'sell_timestamp'], coin_balance[valid_sell_timestamps], label='BTC Balance Over Time', color='orange')
+    axs[3].set(ylabel='Balance')
+    axs[3].grid(True)
+    axs[3].legend()
+
+    plt.show()
+
+import matplotlib.dates as mdates
+
+def plot_trade_history(trade_history, coin_data_1h, coin):
+    fig, ax = plt.subplots(2, 1, figsize=(10, 12), sharex=True)
+
+    # Convert Timestamp to datetime
+    trade_history['buy_timestamp'] = pd.to_datetime(trade_history['buy_timestamp']).dt.to_pydatetime()
+    trade_history['sell_timestamp'] = pd.to_datetime(trade_history['sell_timestamp']).dt.to_pydatetime()
+
+    # Filter out NaT values
+    valid_sell_timestamps = trade_history['sell_timestamp'].notna()
+
+    # Plot strategy on the same graph
+    ax[0].plot(coin_data_1h.index, coin_data_1h['Close'], label=f'{coin}-USD Close Price', color='blue')
+    ax[0].plot(coin_data_1h.index, coin_data_1h['MA200'], label='200 Minute MA', color='red')
+    ax[0].plot(coin_data_1h[coin_data_1h['Buy_Signal']].index, coin_data_1h['Close'][coin_data_1h['Buy_Signal']], '^', markersize=3, color='g', label='buy')
+    ax[0].plot(coin_data_1h[coin_data_1h['Sell_Signal']].index, coin_data_1h['Close'][coin_data_1h['Sell_Signal']], 'v', markersize=3, color='r', label='sell')
+    ax[0].set(xlabel='Date', ylabel='Close Price')
+    ax[0].grid(True)
+    ax[0].legend()
+
+    # Set x-axis to display hourly ticks
+    ax[0].xaxis.set_major_locator(mdates.HourLocator(interval=1))  # set ticks to display every hour
+    ax[0].xaxis.set_major_formatter(mdates.DateFormatter('%H:%M'))  # set ticks format to display hour and minute
+
+    # Plot buy and sell amounts on the first graph
+    ax[1].bar(trade_history['buy_timestamp'], trade_history['buy_amount'], label='Coin Amount Bought Over Time', color='green')
+    # ax[1].bar(trade_history.loc[valid_sell_timestamps, 'sell_timestamp'], trade_history.loc[valid_sell_timestamps, 'sell_amount'], label='Coin Amount Sold Over Time', color='orange')
+    ax[1].set(ylabel='Coin Amount')
+    ax[1].grid(True)
+    ax[1].legend()
+
+    # Set x-axis to display hourly ticks
+    ax[1].xaxis.set_major_locator(mdates.HourLocator(interval=1))  # set ticks to display every hour
+    ax[1].xaxis.set_major_formatter(mdates.DateFormatter('%H:%M'))  # set ticks format to display hour and minute
+
+    plt.tight_layout()
     plt.show()
 
 coin = 'BTC'
-coin_data_1h = download_currency_data(coin, days_to_download=730, interval='1h')
+coin_data_1h = download_currency_data(coin, days_to_download=20, interval='1h')
 coin_data_1h = calculate_strategy_1(coin_data_1h)
 
 usd_balance, coin_balance, trade_history = backtest(coin_data_1h)
 
-plot_trade_history(trade_history, coin_data_1h, coin)
+plot_trade_history(trade_history,coin_data_1h, coin)
 
 
--- backtest.py
+++ backtest.py
@@ -1,5 +1,4 @@
 from tqdm import tqdm
-from trader import KrakenAPI
 from datetime import datetime, timedelta
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
@@ -7,60 +6,95 @@ import pandas as pd
 
 import yfinance as yf
 
-# Download historical data for BTC
-end = datetime.today()
-start = end - timedelta(days=730)
-btc = yf.download('BTC-USD', start=start, end=end, interval='1h')
-
-# Calculate the 200 hour moving average
-btc['MA200'] = btc['Close'].rolling(window=200).mean()
-
-# Define the strategy
-btc['Buy_Signal'] = (btc['Close'] < btc['MA200'])
-btc['Sell_Signal'] = (btc['Close'] > btc['MA200'])
-
-# Plot the closing prices and moving average
-plt.figure(figsize=(10, 6))
-plt.plot(btc.index, btc['Close'], label='BTC-USD Close Price', color='blue')
-plt.plot(btc.index, btc['MA200'], label='200 Hour MA', color='red')
-
-# Plot the buy signals
-plt.plot(btc[btc['Buy_Signal']].index, btc['Close'][btc['Buy_Signal']], '^', markersize=3, color='g', label='buy')
-
-# Plot the sell signals
-plt.plot(btc[btc['Sell_Signal']].index, btc['Close'][btc['Sell_Signal']], 'v', markersize=3, color='r', label='sell')
-
-plt.xlabel('Date')
-plt.ylabel('Close Price')
-plt.title('BTC-USD Close Price with Buy/Sell Signals')
-plt.grid(True)
-plt.legend()
-plt.show()
+def download_currency_data(currency='BTC', days_to_download=30, interval='1h'):
+    end = datetime.today()
+    start = end - timedelta(days=days_to_download)
+    data = yf.download(f'{currency}-USD', start=start, end=end, interval=interval)
+    if data.empty:
+        print(f"Error occurred: No data was downloaded for {currency}")
+    else:
+        data = data.drop_duplicates().sort_index()
+    return data
 
 
-# Initialize the balance in USD and BTC, and the purchases list
-usd_balance = 1000.0
-btc_balance = 0.0
-purchases = []
+def calculate_strategy_1(data):
+    # Calculate the 200 hour moving average
+    data['MA200'] = data['Close'].rolling(window=200).mean()
 
-# Iterate over each row in the DataFrame
-for i, row in tqdm(btc.iterrows(), total=btc.shape[0]):
-    # If there is a buy signal and we have enough USD balance, we buy
-    if row['Buy_Signal'] and usd_balance >= row['Close']:
-        btc_amount = usd_balance / row['Close']
-        purchases.append({'price': row['Close'], 'date': i, 'btc_amount': btc_amount})
-        usd_balance -= row['Close']
-        btc_balance += btc_amount
+    # Define the strategy
+    data['Buy_Signal'] = (data['Close'] < data['MA200'])
+    data['Sell_Signal'] = (data['Close'] > data['MA200'])
+    return data
 
-    # If there is a sell signal, we sell the oldest purchase with at least a 1% gain
-    elif row['Sell_Signal'] and purchases:
-        for purchase in purchases:
-            if row['Close'] > purchase['price'] * 1.01:
-                usd_balance += purchase['btc_amount'] * row['Close']  # We add the value of the purchase plus the gain
-                btc_balance -= purchase['btc_amount']
-                purchases.remove(purchase)
-                break
+from tqdm import tqdm
 
-# Print the final balances
-print("Final USD Balance: ", usd_balance)
-print("Final BTC Balance: ", btc_balance)
+def backtest(data, usd_balance=10000.0, coin_balance=0.0, buy_amount=10):
+    # Crear un DataFrame vacío para almacenar las compras
+    purchases = pd.DataFrame(columns=['Timestamp', 'Buy_Amount_USD', 'Quantity_BTC', 'Balance_USD', 'Balance_BTC', 'Total_Value_USD'])
+
+    for i, row in tqdm(data.iterrows(), total=data.shape[0]):
+        # Comprar
+        if row['Buy_Signal'] and usd_balance >= buy_amount:
+            quantity = buy_amount / row['Close']
+            coin_balance += quantity
+            usd_balance -= buy_amount
+            # Calcular el balance total en USD y el valor total
+            total_value = usd_balance + coin_balance * row['Close']
+            # Agregar la compra al DataFrame
+            new_purchase = pd.DataFrame(
+                {
+                    'Timestamp': [i], 
+                    'Buy_Amount_USD': [buy_amount],
+                    'Quantity_BTC': [quantity], 
+                    'Balance_USD': [usd_balance],
+                    'Balance_BTC': [coin_balance],
+                    'Total_Value_USD': [total_value]
+                }
+            )
+            purchases = pd.concat([purchases, new_purchase], ignore_index=True)
+
+    return purchases
+
+def plot_data(data, purchases):
+    fig, ax = plt.subplots(2, 1, figsize=(10, 12), sharex=True)
+
+    # Plot strategy on the first graph
+    ax[0].plot(data.index, data['Close'], label='Close Price', color='blue')
+    ax[0].plot(data.index, data['MA200'], label='200 Hour MA', color='red')
+    ax[0].plot(data[data['Buy_Signal']].index, data['Close'][data['Buy_Signal']], '^', markersize=3, color='g', label='buy')
+    ax[0].plot(data[data['Sell_Signal']].index, data['Close'][data['Sell_Signal']], 'v', markersize=3, color='r', label='sell')
+    ax[0].set(xlabel='Date', ylabel='Close Price')
+    ax[0].grid(True)
+    ax[0].legend()
+
+    # Convert Timestamp to datetime
+    purchases['Timestamp'] = pd.to_datetime(purchases['Timestamp']).dt.to_pydatetime()
+
+    # Plot balances and total value on the second graph
+    ax[1].plot(purchases['Timestamp'], purchases['Balance_USD'], label='USD Balance', color='green')
+
+    # Create a second y-axis for the BTC balance
+    ax2 = ax[1].twinx()
+    ax2.plot(purchases['Timestamp'], purchases['Balance_BTC'], label='BTC Balance', color='orange')
+
+    ax[1].plot(purchases['Timestamp'], purchases['Total_Value_USD'], label='Total Value in USD', color='blue')
+    ax[1].set(ylabel='Amount (USD)')
+    ax2.set_ylabel('Amount (BTC)')
+    ax[1].grid(True)
+
+    # Add legends for both y-axes
+    ax[1].legend(bbox_to_anchor=(0.88, 1), loc='upper left')
+    ax2.legend(bbox_to_anchor=(0.88, 0.9), loc='upper left')
+
+    plt.tight_layout()
+    plt.show()
+
+
+coin = 'BTC'
+coin_data_1h = download_currency_data(coin, days_to_download=730, interval='1h')
+coin_data_1h_signals = calculate_strategy_1(coin_data_1h)
+purchases = backtest(coin_data_1h_signals)
+print()
+plot_data(coin_data_1h_signals, purchases)
+
+
