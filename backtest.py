cf7be83476a5be66dd7d40334f6c77c15d3fe656
Brunvelop <brunvelop@gmail.com>
2023-09-09 17:11:00 +0200

    add new strategies

--- backtest.py
+++ backtest.py
@@ -39,9 +39,33 @@ def calculate_strategy_2(data):
     
     return data
 
-def backtest(data, usd_balance=10000.0, coin_balance=0.0, buy_amount=10):
-    purchases = pd.DataFrame(columns=['Buy_Timestamp', 'Buy_Amount_USD', 'Buy_Quantity_BTC', 'Balance_USD', 'Balance_BTC', 'Total_Value_USD'])
+def calculate_strategy_3(data):
+    # Calculate the moving averages
+    data['MA10'] = data['Close'].rolling(window=10).mean()
+    data['MA50'] = data['Close'].rolling(window=50).mean()
+    data['MA100'] = data['Close'].rolling(window=100).mean()
+    data['MA200'] = data['Close'].rolling(window=200).mean()
 
+    # Calculate the bar range as a percentage of the close price
+    data['BarRange'] = abs(data['High'] - data['Low']) / data['Low'] * 100
+
+    # Calculate the exponential moving averages of the BarRange
+    data['avgRange10'] = data['BarRange'].ewm(span=10).mean()
+    data['avgRange50'] = data['BarRange'].ewm(span=50).mean()
+    data['avgRange100'] = data['BarRange'].ewm(span=100).mean()
+    data['avgRange200'] = data['BarRange'].ewm(span=200).mean()
+
+    # Define the strategy
+    data['Buy_Signal'] = (data['Close'] < data[['MA10', 'MA50', 'MA100', 'MA200']].min(axis=1)) & (data['MA10'] < data['MA50']) & (data['MA50'] < data['MA100']) & (data['MA100'] < data['MA200']) & (data['avgRange10'] < data['avgRange50']) & (data['avgRange50'] < data['avgRange100']) & (data['avgRange100'] < data['avgRange200'])
+    data['Sell_Signal'] = (data['Close'] > data[['MA10', 'MA50', 'MA100', 'MA200']].max(axis=1)) & (data['MA10'] > data['MA50']) & (data['MA50'] > data['MA100']) & (data['MA100'] > data['MA200'])
+
+    return data
+
+def backtest(data, usd_balance=10000.0, coin_balance=0.0, buy_amount=10):
+    purchases = pd.DataFrame(columns=['Timestamp', 'Buy_Amount_USD', 'Quantity_BTC', 'Balance_USD', 'Balance_BTC', 'Total_Value_USD', 'Is_Sold', 'Sell_Amount_USD', 'Sell_Quantity_BTC', 'Sell_Timestamp'])
+    balances = pd.DataFrame(columns=['Timestamp', 'Balance_USD', 'Balance_BTC', 'Total_Value_USD'])
+    total_value = usd_balance
+    
     for i, row in tqdm(data.iterrows(), total=data.shape[0]):
         # Comprar
         if row['Buy_Signal'] and usd_balance >= buy_amount:
@@ -56,7 +80,11 @@ def backtest(data, usd_balance=10000.0, coin_balance=0.0, buy_amount=10):
                     'Quantity_BTC': [quantity], 
                     'Balance_USD': [usd_balance],
                     'Balance_BTC': [coin_balance],
-                    'Total_Value_USD': [total_value]
+                    'Total_Value_USD': [total_value],
+                    'Is_Sold': 0,
+                    'Sell_Amount_USD': [None],
+                    'Sell_Quantity_BTC': [None],
+                    'Sell_Timestamp': [None]
                 }
             )
             purchases = pd.concat([purchases, new_purchase], ignore_index=True)
@@ -64,21 +92,34 @@ def backtest(data, usd_balance=10000.0, coin_balance=0.0, buy_amount=10):
         elif row['Sell_Signal'] and coin_balance > 0:
             # Buscar la compra más antigua con al menos un 2% de rentabilidad
             for j, purchase in purchases.iterrows():
-                if (row['Close'] - purchase['Buy_Amount_USD']) / purchase['Buy_Amount_USD'] >= 0.02:
+                if (row['Close'] - purchase['Buy_Amount_USD']) / purchase['Buy_Amount_USD'] >= 0.02 and not purchase['Is_Sold']:
                     # Vender
-                    usd_balance += purchase['Quantity_BTC'] * row['Close']
-                    coin_balance -= purchase['Quantity_BTC']
+                    sell_amount = purchase['Quantity_BTC'] * row['Close']
+                    sell_quantity = purchase['Quantity_BTC']
+                    usd_balance += sell_amount
+                    coin_balance -= sell_quantity
                     total_value = usd_balance + coin_balance * row['Close']
                     # Actualizar el DataFrame de compras
-                    # Actualizar el DataFrame de compras
-                    purchases.at[j, 'Balance_USD'] = usd_balance
-                    purchases.at[j, 'Balance_BTC'] = coin_balance
-                    purchases.at[j, 'Total_Value_USD'] = total_value
+                    purchases.at[j, 'Is_Sold'] = 1
+                    purchases.at[j, 'Sell_Amount_USD'] = sell_amount
+                    purchases.at[j, 'Sell_Quantity_BTC'] = sell_quantity
+                    purchases.at[j, 'Sell_Timestamp'] = i
                     break
 
-    return purchases
+        # Almacenar balances
+        new_balance = pd.DataFrame(
+            {
+                'Timestamp': [i],
+                'Balance_USD': [usd_balance],
+                'Balance_BTC': [coin_balance],
+                'Total_Value_USD': [total_value]
+            }
+        )
+        balances = pd.concat([balances, new_balance], ignore_index=True)
 
-def plot_data(data, purchases):
+    return purchases, balances
+
+def plot_data(data, purchases, balances):
     fig, ax = plt.subplots(2, 1, figsize=(10, 12), sharex=True)
 
     # Plot strategy on the first graph
@@ -97,13 +138,13 @@ def plot_data(data, purchases):
     purchases['Timestamp'] = pd.to_datetime(purchases['Timestamp']).dt.to_pydatetime()
 
     # Plot balances and total value on the second graph
-    ax[1].plot(purchases['Timestamp'], purchases['Balance_USD'], label='USD Balance', color='green')
+    ax[1].plot(balances['Timestamp'], balances['Balance_USD'], label='USD Balance', color='green')
 
     # Create a second y-axis for the BTC balance
     ax2 = ax[1].twinx()
-    ax2.plot(purchases['Timestamp'], purchases['Balance_BTC'], label='BTC Balance', color='orange')
+    ax2.plot(balances['Timestamp'], balances['Balance_BTC'], label='BTC Balance', color='orange')
 
-    ax[1].plot(purchases['Timestamp'], purchases['Total_Value_USD'], label='Total Value in USD', color='blue')
+    ax[1].plot(balances['Timestamp'], balances['Total_Value_USD'], label='Total Value in USD', color='blue')
     ax[1].set(ylabel='Amount (USD)')
     ax2.set_ylabel('Amount (BTC)')
     ax[1].grid(True)
@@ -118,9 +159,9 @@ def plot_data(data, purchases):
 
 coin = 'BTC'
 coin_data_1h = download_currency_data(coin, days_to_download=730, interval='1h')
-coin_data_1h_signals = calculate_strategy_2(coin_data_1h)
-purchases = backtest(coin_data_1h_signals, buy_amount=1)
+coin_data_1h_signals = calculate_strategy_3(coin_data_1h)
+purchases, balances = backtest(coin_data_1h_signals, buy_amount=100)
 print()
-plot_data(coin_data_1h_signals, purchases)
+plot_data(coin_data_1h_signals, purchases, balances)
 
 
