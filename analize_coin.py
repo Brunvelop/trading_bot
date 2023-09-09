a60eff450d73912466736c265680973cda5761f6
Brunvelop <brunvelop@gmail.com>
2023-09-09 11:18:26 +0200

    Current

--- analize_coin.py
+++ analize_coin.py
@@ -34,76 +34,91 @@ def calculate_strategy_1(data):
     return data
 
 def backtest(data, usd_balance=10000.0, coin_balance=0.0, buy_amount=100):
-    trade_history = pd.DataFrame(columns=['order_id', 'buy_timestamp', 'buy_price', 'buy_amount', 'buy_cost', 'buy_fees', 'closed', 'sell_timestamp', 'sell_price', 'sell_amount', 'sell_cost', 'sell_fees'])
+    trade_history = []
     fee_rate = 0.0012  # 0.12% fee
     order_id = 0
+    open_orders = []
+
     for i, row in tqdm(data.iterrows(), total=data.shape[0]):
+        fee = buy_amount * fee_rate
         # If there is a buy signal, we buy one unit of coin
-        if row['Buy_Signal'] and usd_balance >= buy_amount/row['Close']:
+        if row['Buy_Signal'] and usd_balance >= (buy_amount + fee):
+            buy_amount -= fee
             coin_amount = buy_amount/row['Close']
             usd_amount = buy_amount
-            fee = usd_amount * fee_rate
+
             coin_balance += coin_amount
-            usd_balance -= usd_amount + fee
-            trade_history = trade_history.append(
-                {
-                    'order_id': order_id, 
-                    'buy_timestamp': row.name, 
-                    'buy_price': row['Close'], 
-                    'buy_amount': coin_amount, 
-                    'buy_cost': usd_amount, 
-                    'buy_fees': fee, 
-                    'closed': False
-                }, 
-                ignore_index=True
-            )
+            usd_balance -= usd_amount
+            trade = {
+                'order_id': order_id, 
+                'buy_timestamp': row.name, 
+                'buy_price': row['Close'], 
+                'buy_amount': coin_amount, 
+                'buy_cost': usd_amount, 
+                'buy_fees': fee, 
+                'closed': False
+            }
+            trade_history.append(trade)
+            open_orders.append(trade)
             order_id += 1
 
         # If there is a sell signal and we have coin balance, we sell
         elif row['Sell_Signal'] and coin_balance > 0:
             # Find the oldest purchase with at least 2% profitability
-            for j, trade in trade_history.iterrows():
-                if not trade['closed'] and row['Close'] > trade['buy_price'] * 1.02:
+            for trade in open_orders:
+                if row['Close'] > trade['buy_price'] * 1.02:
                     coin_amount = trade['buy_amount']
                     usd_amount = coin_amount * row['Close']
                     fee = usd_amount * fee_rate
                     usd_balance += usd_amount - fee
                     coin_balance -= coin_amount
-                    trade_history.loc[j, 'sell_timestamp'] = row.name
-                    trade_history.loc[j, 'sell_price'] = row['Close']
-                    trade_history.loc[j, 'sell_amount'] = coin_amount
-                    trade_history.loc[j, 'sell_cost'] = usd_amount
-                    trade_history.loc[j, 'sell_fees'] = fee
-                    trade_history.loc[j, 'closed'] = True
+                    trade.update({
+                        'sell_timestamp': row.name,
+                        'sell_price': row['Close'],
+                        'sell_amount': coin_amount,
+                        'sell_cost': usd_amount,
+                        'sell_fees': fee,
+                        'closed': True
+                    })
+                    open_orders.remove(trade)
                     break
-            
+
+    trade_history = pd.DataFrame(trade_history)
     return usd_balance, coin_balance, trade_history
 
+def plot_trade_history(trade_history, coin_data_1h, coin):
+    fig, axs = plt.subplots(3, 1, figsize=(10, 18))
+    fig.suptitle('Trade History Over Time')
+
+    # Filter closed trades
+    closed_trades = trade_history[trade_history['closed']]
+
+    axs[0].plot(trade_history['buy_timestamp'], trade_history['buy_cost'], label='Buy Cost Over Time', color='blue')
+    axs[0].set(ylabel='Buy Cost')
+    axs[0].grid(True)
+    axs[0].legend()
+
+    axs[1].plot(closed_trades['sell_timestamp'], closed_trades['sell_cost'], label='Sell Cost Over Time', color='green')
+    axs[1].set(ylabel='Sell Cost')
+    axs[1].grid(True)
+    axs[1].legend()
+
+    axs[2].plot(coin_data_1h.index, coin_data_1h['Close'], label=f'{coin}-USD Close Price', color='blue')
+    axs[2].plot(coin_data_1h.index, coin_data_1h['MA200'], label='200 Minute MA', color='red')
+    axs[2].plot(coin_data_1h[coin_data_1h['Buy_Signal']].index, coin_data_1h['Close'][coin_data_1h['Buy_Signal']], '^', markersize=3, color='g', label='buy')
+    axs[2].plot(coin_data_1h[coin_data_1h['Sell_Signal']].index, coin_data_1h['Close'][coin_data_1h['Sell_Signal']], 'v', markersize=3, color='r', label='sell')
+    axs[2].set(xlabel='Date', ylabel='Close Price')
+    axs[2].grid(True)
+    axs[2].legend()
+
+    plt.show()
+
 coin = 'BTC'
-coin_data_1h = download_currency_data(coin, days_to_download=730, interval='1m')
+coin_data_1h = download_currency_data(coin, days_to_download=730, interval='1h')
 coin_data_1h = calculate_strategy_1(coin_data_1h)
+
 usd_balance, coin_balance, trade_history = backtest(coin_data_1h)
 
-fig, axs = plt.subplots(3, 1, figsize=(10, 18))
-fig.suptitle('USD and Coin Balance Over Time')
+plot_trade_history(trade_history, coin_data_1h, coin)
 
-axs[0].plot(balance_history['Date'], balance_history['USD_Balance'], label='USD Balance Over Time', color='blue')
-axs[0].set(ylabel='USD Balance')
-axs[0].grid(True)
-axs[0].legend()
-
-axs[1].plot(balance_history['Date'], balance_history['Coin_Balance'], label='Coin Balance Over Time', color='green')
-axs[1].set(ylabel='Coin Balance')
-axs[1].grid(True)
-axs[1].legend()
-
-axs[2].plot(coin_data_1h.index, coin_data_1h['Close'], label=f'{coin}-USD Close Price', color='blue')
-axs[2].plot(coin_data_1h.index, coin_data_1h['MA200'], label='200 Minute MA', color='red')  # Changed label to '200 Minute MA'
-axs[2].plot(coin_data_1h[coin_data_1h['Buy_Signal']].index, coin_data_1h['Close'][coin_data_1h['Buy_Signal']], '^', markersize=3, color='g', label='buy')
-axs[2].plot(coin_data_1h[coin_data_1h['Sell_Signal']].index, coin_data_1h['Close'][coin_data_1h['Sell_Signal']], 'v', markersize=3, color='r', label='sell')
-axs[2].set(xlabel='Date', ylabel='Close Price')
-axs[2].grid(True)
-axs[2].legend()
-
-plt.show()
 
