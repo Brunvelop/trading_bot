25d7121da1e2654d31772a58d68b6c684b8159d4
Brunvelop <brunvelop@gmail.com>
2023-10-06 10:53:24 +0200

    Refactored database connection to use Supabase instead of SQLite.

--- db.py
+++ db.py
@@ -1,110 +1,62 @@
-import sqlite3
-from sqlite3 import Error
+import os
+from supabase_py import create_client, Client
+from dotenv import load_dotenv
+
 
 class DB():
+    def __init__(self):
+        load_dotenv()
+        self.supabase: Client = create_client(
+            os.getenv('SUPABASE_URL'),
+            os.getenv('SUPABASE_KEY')
+        )
 
-    def __init__(self, db_name='db.db'):
-        self.db_name = db_name
-        self.create_table()
-
-    def sql_connection(self):
-        try:
-            conexion = sqlite3.connect(self.db_name)
-            return conexion
-        except Error as e:
-            print(f"Error establishing connection to database: {e}")
-
-    def create_table(self):
-        conexion = self.sql_connection()
-        cursor = conexion.cursor()
-        create_table_query = """
-            CREATE TABLE IF NOT EXISTS trades (
-                order_id TEXT, 
-                buy_timestamp INT, 
-                buy_price REAL, 
-                buy_amount REAL, 
-                buy_cost REAL, 
-                buy_fees REAL,
-                closed BOOL,
-                sell_timestamp INT, 
-                sell_price REAL, 
-                sell_amount REAL, 
-                sell_cost REAL,  
-                sell_fees REAL
-            );
-        """
-        cursor.execute(create_table_query)
-        conexion.commit()
-        print("Table 'trades' created successfully")
-        cursor.close()
-        conexion.close()
-
-    def insert_order(self, order_id, timestamp, price, amount, cost, fees, closed):
-        conexion = self.sql_connection()
-        cursor = conexion.cursor()
-        insert_query = """
-            INSERT INTO trades (order_id, buy_timestamp, buy_price, buy_amount, buy_cost, buy_fees, closed)
-            VALUES (?, ?, ?, ?, ?, ?, ?)
-        """
-        values = (order_id, timestamp, price, amount, cost, fees, closed)
-        cursor.execute(insert_query, values)
-        conexion.commit()
-        print("Data inserted successfully")
-        cursor.close()
-        conexion.close()
+    def insert_order(self, timestamp, price, amount, cost, fees, closed):
+        data = self.supabase.table('trades').insert([{
+            'buy_timestamp': timestamp,
+            'buy_price': price,
+            'buy_amount': amount,
+            'buy_cost': cost,
+            'buy_fees': fees,
+            'closed': closed
+        }]).execute()
+        return data
 
     def get_order_by_id(self, order_id):
-        conexion = self.sql_connection()
-        cursor = conexion.cursor()
-        query = """
-            SELECT * FROM trades WHERE order_id = ?
-        """
-        cursor.execute(query, (order_id,))
-        orders = cursor.fetchall()
-        cursor.close()
-        conexion.close()
-        return orders
+        return self.supabase.table('trades').select().filter('order_id', 'eq', order_id).execute()
 
     def get_orders_below(self, price):
-        conexion = self.sql_connection()
-        cursor = conexion.cursor()
-        query = """
-            SELECT * FROM trades WHERE buy_price < ? AND closed = 0 ORDER BY buy_price DESC
-        """
-        cursor.execute(query, (price,))
-        orders = cursor.fetchall()
-        cursor.close()
-        conexion.close()
-        return orders
+        return self.supabase.table('trades').select().filter('buy_price', 'lt', price).filter('closed', 'eq', 0).order('buy_price', ascending=False).execute()
 
     def get_all_orders(self):
-        conexion = self.sql_connection()
-        cursor = conexion.cursor()
-        query = """
-            SELECT * FROM trades
-        """
-        cursor.execute(query)
-        orders = cursor.fetchall()
-        cursor.close()
-        conexion.close()
-        return orders
+        return self.supabase.table('trades').select().execute()
 
     def update_order(self, order_id, sell_timestamp, sell_price, sell_amount, sell_cost, sell_fees):
-        conexion = self.sql_connection()
-        cursor = conexion.cursor()
-        update_query = """
-            UPDATE trades SET 
-            closed = 1, 
-            sell_timestamp = ?, 
-            sell_price = ?, 
-            sell_amount = ?, 
-            sell_cost = ?, 
-            sell_fees = ?
-            WHERE order_id = ?
-        """
-        values = (sell_timestamp, sell_price, sell_amount, sell_cost, sell_fees, order_id)
-        cursor.execute(update_query, values)
-        conexion.commit()
-        print(f"Order {order_id} updated successfully")
-        cursor.close()
-        conexion.close()
+        self.supabase.table('trades').update({
+            'closed': True,
+            'sell_timestamp': sell_timestamp,
+            'sell_price': sell_price,
+            'sell_amount': sell_amount,
+            'sell_cost': sell_cost,
+            'sell_fees': sell_fees
+        }).match({'order_id': order_id}).execute()
+    
+    def check_table(self):
+        tables = self.supabase.rpc('pg_catalog', 'pg_tables')
+        table_names = [table['tablename'] for table in tables['data']]
+        if 'trades' not in table_names:
+            print("Table 'trades' does not exist. Please create it via the Supabase web interface.")
+
+
+if __name__ == '__main__':
+    import datetime
+    db = DB()
+    a = db.insert_order(
+        timestamp=datetime.datetime.fromtimestamp(1696436779).isoformat(),
+        price=45000.00,
+        amount=0.001,
+        cost=45.00,
+        fees=0.01,
+        closed=False
+    )
+    a
---
+++ trader2.py
@@ -0,0 +1,166 @@
+import os
+from dotenv import load_dotenv
+
+import time
+import ccxt
+
+from db import DB
+
+load_dotenv()
+
+
+class KrakenAPI:
+    def __init__(self):
+        pass
+
+    def connect_api(self):
+        exchange = ccxt.kraken({
+            'apiKey': os.getenv('KRAKEN_API_KEY'),
+            'secret': os.getenv('KRAKEN_API_SECRET'),
+            'enableRateLimit': True,
+            'options':{
+                'defaultType': 'spot',
+            }
+        })
+        return exchange
+
+    def get_latest_price(self, pair):
+        exchange = self.connect_api()
+        ticker = exchange.fetch_ticker(pair)
+        return ticker['last']
+    
+    def get_account_balance(self, currency):
+        exchange = self.connect_api()
+        balance = exchange.fetch_balance()
+        return balance['total'][currency]
+
+    def get_bars(self, pair, timeframe, limit):
+        exchange = self.connect_api()
+        return exchange.fetch_ohlcv(pair, timeframe=timeframe, limit=limit)[::-1]
+
+    def get_order(self, order_id):
+        exchange = self.connect_api()
+        return exchange.fetch_order(order_id)
+
+    def create_order(self, pair, order_type, side, amount, price):
+        exchange = self.connect_api()
+        return exchange.create_order(pair, order_type, side, amount, price)
+
+    def get_smas(self, bars, periods=(10, 50, 100, 200)):
+        smas = []
+        sma_totals = [0] * len(periods)
+        max_period = max(periods)
+
+        for i, bar in enumerate(bars[:max_period], start=1):
+            price = bar[1]
+            for j, period in enumerate(periods):
+                if i < period:
+                    sma_totals[j] += price
+                elif i == period:
+                    smas.append(sma_totals[j] / period)
+
+        return smas
+
+
+class Trader:
+    def __init__(self, pair='BTC/USD', cost=5, time_period='1h', gain_threshold=0.05):
+        self.kraken_api = KrakenAPI()
+        self.pair = pair
+        self.cost = cost
+        self.time_period = time_period
+        self.gain_threshold = gain_threshold
+        self.db = DB()
+
+    def get_amount(self, price):
+        return self.cost / price
+
+    def run_strategy(self):
+        bars = self.kraken_api.get_bars(self.pair, self.time_period, 200)
+        sma_10, sma_50, sma_100, sma_200 = self.kraken_api.get_smas(bars)
+        price = self.kraken_api.get_latest_price(self.pair)
+        
+        print("price:", price, "smas(10,50,100,200)=", sma_10, sma_50, sma_100, sma_200)
+        if price > sma_10 > sma_50 > sma_100 > sma_200:
+            print('Trying to sell...', flush=True)
+            self.sell()
+        elif price < sma_10 < sma_50 < sma_100 < sma_200:
+            print('Buying...', flush=True)
+            self.buy()
+        else:
+            print('Waiting...', flush=True)
+
+    def buy(self):
+        price = self.kraken_api.get_latest_price(self.pair)
+        amount = self.get_amount(price)
+        order = self.kraken_api.create_order(self.pair, 'market', 'buy', amount, price)
+        order_executed_info = self.get_order_info(order['id'])
+        self.db.insert_order(
+                order_executed_info['id'],
+                order_executed_info['timestamp'],
+                order_executed_info['price'],
+                order_executed_info['amount'],
+                order_executed_info['cost'],
+                order_executed_info['fees'],
+                False
+            )
+        return self.get_order_info(order['id'])
+
+    def sell(self):
+        price = self.kraken_api.get_latest_price(self.pair)
+        orders = self.db.get_orders_below(price*(1-self.gain_threshold))
+        if orders:
+            amount = orders[0][3]
+            order = self.kraken_api.create_order(self.pair, 'market', 'sell', amount, price)
+            order_info = self.get_order_info(order['id'])
+            self.db.update_order(
+                    orders[0][0],
+                    order_info['timestamp'],
+                    order_info['price'],
+                    order_info['amount'],
+                    order_info['cost'],
+                    order_info['fees'],
+            )
+            return order_info
+        return None
+
+    def sell_to_market(self, order_id):
+        order = self.db.get_order_by_id(order_id)[0]
+        amount = order[3]
+        price = self.kraken_api.get_latest_price(self.pair)
+        order_new = self.kraken_api.create_order(self.pair, 'market', 'sell', amount, price)
+        order_new_info = self.get_order_info(order_new['id'])
+        self.db.update_order(
+                    order_id,
+                    order_new_info['timestamp'],
+                    order_new_info['price'],
+                    order_new_info['amount'],
+                    order_new_info['cost'],
+                    order_new_info['fees'],
+            )
+        return order_new_info
+
+    def get_order_info(self, order_id):
+        order = self.kraken_api.get_order(order_id)
+        return {
+            'id': order['id'],
+            'timestamp': order['timestamp'],
+            'price': order['price'], 
+            'amount': order['amount'], 
+            'cost': order['cost'], 
+            'fees': order['fees'][0]['cost'],
+        }
+
+
+import timeit
+import pandas as pd
+
+
+
+# Crea una instancia de la clase que contiene las funciones
+kraken_api = KrakenAPI()
+
+pair='BTC/USD'
+time_period='15m'
+bars = kraken_api.get_bars(pair, time_period, 200)
+
+sma_10, sma_50, sma_100, sma_200 = kraken_api.get_smas(bars)
