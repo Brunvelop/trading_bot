60c1756a6111e2a886e85abb6617e4a1c7bd6704
Brunvelop <brunvelop@gmail.com>
2024-07-22 20:18:08 +0200

    refactor and clean strategy

--- strategies.py
+++ strategies.py
@@ -1,11 +1,12 @@
+from enum import Enum, auto
+from datetime import datetime
 from typing import Tuple, List
 from abc import ABC, abstractmethod
-from datetime import datetime
 
 import pandas as pd
 
 from indicators import Indicators
-from definitions import Action, Memory, MarketData, OscilationAnalysis
+from definitions import Action, Memory, MarketData, TradingPhase, OscilationAnalysis
 
 
 class Strategy(ABC):
@@ -36,53 +37,83 @@ class MovingAverageStrategy(Strategy):
         return actions
     
 class MultiMovingAverageStrategy(Strategy):
-    def __init__(self, ab_ratio: float = 1, max_duration: int = 500, min_purchase:float = 5,
-                 safety_margin: float = 3, windows: List[int] = [10, 50, 100, 200],
+    class Alignment(Enum):
+        UP = auto()
+        DOWN = auto()
+        NONE = auto()
+
+    def __init__(self, max_duration: int = 500, 
+                 min_purchase:float = 5.1,
+                 safety_margin: float = 3, 
+                 windows: List[int] = [10, 50, 100, 200],
+                 trading_phase: TradingPhase = TradingPhase.NEUTRAL,
                  debug: bool = True) -> None:
-        self.windows = windows
-        self.ab_ratio = ab_ratio
         self.max_duration = max_duration
-        self.safety_margin = safety_margin
         self.min_purchase = min_purchase
+        self.safety_margin = safety_margin
+        self.windows = windows
         self.distribution_length = 0
         self.acumulation_length = 0
+        self.trading_phase = trading_phase
         self.debug = debug
+    
+    def _determine_alignment(self, data: MarketData) -> Alignment:
+        moving_averages = [Indicators.calculate_moving_average(data, window).iloc[-1] for window in self.config['windows']]
+        current_price = data['Close'].iloc[-1]
         
+        if current_price > moving_averages[0] > moving_averages[1] > moving_averages[2] > moving_averages[3]:
+            return self.Alignment.UP
+        elif current_price < moving_averages[0] < moving_averages[1] < moving_averages[2] < moving_averages[3]:
+            return self.Alignment.DOWN
+        return self.Alignment.NONE
+
+    def _calculate_amount(self, balance_a: float, balance_b: float, current_price: float) -> float:
+        if self.trading_phase == TradingPhase.NEUTRAL:
+            return 0
+        elif self.trading_phase == TradingPhase.ACCUMULATION:
+            amount = balance_b / (self.max_duration * self.safety_margin * current_price)
+        elif self.trading_phase == TradingPhase.DISTRIBUTION:
+            amount = balance_a / ( self.max_duration * self.safety_margin )
+
+        return max(amount, self.min_purchase)
+
+    def _can_sell(self, balance_a: float, amount: float) -> bool:
+        enough_amount_to_sell = balance_a > amount
+        if self.trading_phase == TradingPhase.ACCUMULATION:
+            return enough_amount_to_sell and self.acumulation_length > 0
+        elif self.trading_phase == TradingPhase.DISTRIBUTION:
+            return enough_amount_to_sell 
+        return enough_amount_to_sell
+    
+    def _can_buy(self, balance_b: float, amount: float, current_price: float) -> bool:
+        enough_amount_to_buy = balance_b > amount * current_price
+        if self.trading_phase == TradingPhase.ACCUMULATION:
+            return enough_amount_to_buy 
+        elif self.trading_phase == TradingPhase.DISTRIBUTION:
+            return enough_amount_to_buy and self.distribution_length > 0
+        return enough_amount_to_buy
+
+
     def run(self, data: MarketData, memory: Memory) -> List[Action]:
         actions = []
-        balance_a = memory.get('balance_a', 0)
-        balance_b = memory.get('balance_b', 0)
+        balance_a, balance_b = memory.get('balance_a', 0), memory.get('balance_b', 0)
         current_price = data['Close'].iloc[-1]
 
-        # Calculamos las medias móviles para cada ventana
-        moving_averages = [Indicators.calculate_moving_average(data, window).iloc[-1] for window in self.windows]
+        alignment = self._determine_alignment(data)
+        amount = self._calculate_amount(balance_a, balance_b, current_price)
 
-        # Comprobamos si las medias móviles están alineadas
-        aligned_up = current_price > moving_averages[0] > moving_averages[1] > moving_averages[2] >  moving_averages[3]
-        aligned_down = current_price < moving_averages[0] < moving_averages[1] < moving_averages[2] <  moving_averages[3]
-
-        # Calculamos que estado
-        acumulation = current_price * balance_a < self.ab_ratio * balance_b 
-        distribution = current_price * balance_a > self.ab_ratio * balance_b
-        
-        if acumulation:
-            amount = balance_b / (self.max_duration * self.safety_margin * current_price)
-            enough_amount_to_sell = balance_a > amount and amount * current_price > self.min_purchase
-            enough_amount_to_buy = balance_b > amount * current_price and amount * current_price > self.min_purchase
-            if aligned_up and enough_amount_to_sell and self.acumulation_length > 0:
+        if self.trading_phase == TradingPhase.ACCUMULATION:
+            if alignment == self.Alignment.UP and self._can_sell(balance_a, amount):
                 self.acumulation_length -=1
                 actions.append((Action.SELL_MARKET, current_price, amount))
-            elif aligned_down and enough_amount_to_buy:
+            elif alignment == self.Alignment.DOWN and self._can_buy(balance_a, amount, current_price):
                 self.acumulation_length +=1
                 actions.append((Action.BUY_MARKET, current_price, amount))
-        elif distribution:
-            amount = balance_a / ( self.max_duration * self.safety_margin )
-            enough_amount_to_sell = balance_a > amount and amount * current_price > self.min_purchase
-            enough_amount_to_buy = balance_b > amount * current_price and amount * current_price > self.min_purchase
-            if aligned_up and enough_amount_to_sell:
+        elif self.trading_phase == TradingPhase.DISTRIBUTION:
+            if alignment == self.Alignment.UP and self._can_sell(balance_a, amount):
                 self.distribution_length +=1
                 actions.append((Action.SELL_MARKET, current_price, amount))
-            elif aligned_down and enough_amount_to_buy and self.distribution_length > 0:
+            elif alignment == self.Alignment.DOWN and self._can_buy(balance_a, amount, current_price):
                 self.distribution_length -=1
                 actions.append((Action.BUY_MARKET, current_price, amount))
         else:
@@ -92,9 +123,9 @@ class MultiMovingAverageStrategy(Strategy):
             print("time:", datetime.fromtimestamp(int(data['Date'].iloc[-1]) / 1000).strftime('%Y-%m-%d %H:%M'))
             print("blance_a:", balance_a,"|", "balance_b:", balance_b)
             print("distribution_n:", self.distribution_length, "acumulation_n:", self.acumulation_length)
-            print("aligned_up:", aligned_up,"|", "aligned_down:", aligned_down)
-            print("acumulation:", acumulation,"|", "distribution:", distribution)
-            print("enough_sell:", enough_amount_to_sell,"|", "enough_buy:", enough_amount_to_buy)
+            print("alignment:", alignment)
+            print("trading phase:", self.trading_phase)
+            print("can_sell:", self._can_sell(balance_a, amount),"|", "can_buy:", self._can_buy(balance_a, amount, current_price))
             print("amount:", amount)
             print("amount_price:", amount * current_price)
             print(actions)
